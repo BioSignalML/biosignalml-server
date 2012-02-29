e40e08087e1c66c41303492c4cbcfab6ad53f192
Dave Brooks <dave@bcs.co.nz>
2012-02-29 22:24:15 +1300

    Allow metadata values to be abbreviate, and do so for units and format.

--- frontend/htmlview.py
+++ frontend/htmlview.py
@@ -62,6 +62,27 @@ def property_details(obj, properties, table, **args):
   return opn + (cls + opn).join(r) + cls
 
 
+BSML_NAMESPACE = 'http://www.biosignalml.org/ontologies/2011/04/biosignalml#'
+
+
+PREFIXES = { 'rdf':  'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+             'rdfs': 'http://www.w3.org/2000/01/rdf-schema#',
+             'xsd':  'http://www.w3.org/2001/XMLSchema#',
+             'owl':  'http://www.w3.org/2002/07/owl#',
+             'dc':   'http://purl.org/dc/elements/1.1/',
+             'dcterms': 'http://purl.org/dc/terms/',
+             'time': 'http://www.w3.org/2006/time#',
+             'bsml':  BSML_NAMESPACE,
+           }
+
+
+def abbreviate(u):
+#-----------------
+  s = str(u) if u else ''
+  for p, n in PREFIXES.iteritems():
+    if s.startswith(n): return ''.join([p, ':', s[len(n):]])
+  return s
+
 
 ############## Above should be in separate module ###############
 
