0da48e38c6b230558d87f367826b71aadb676350
Dave Brooks <dave@bcs.co.nz>
2012-03-07 13:06:44 +1300

    Start handling DATA blocks.

--- frontend/webstream.py
+++ frontend/webstream.py
@@ -87,7 +87,7 @@ class StreamDataSocket(StreamServer):
   def got_block(self, block):
   #--------------------------
     logging.debug('GOT: %s', block)
-    if block.type == stream.BlockType.DATA_REQ:
+    if   block.type == stream.BlockType.DATA_REQ:
       uri = block.header.get('uri')
       self._sigs = [ ]
       if isinstance(uri, list):
@@ -122,6 +122,34 @@ class StreamDataSocket(StreamServer):
           self.send_block(stream.ErrorBlock(0, block, str(msg)))
           if options.debug: raise
       self.close()     ## All done with data request
+
+    elif block.type == stream.BlockType.DATA:
+      # Got 'D' segment(s), uri is that of signal, that should have a recording link
+      # look signal's uri up to get its Recording and hence format/source
+      pass  # append to HDF5 BioSignalML file
+
+      try:
+        sd = block.signaldata()
+        rec = self._repo.get_recording_graph_uri(sd.uri)
+
+        if rec is None or not self._repo.has_signal_in_recording(sd.uri, rec):
+          raise stream.StreamException("Unknown signal '%s'" % sd.uri)
+
+        '''
+        rec format BSML.BioSignalML
+        rec source
+
+        Need to open HDF5 (BSML) Recording and then append data...
+
+
+        '''
+
+      except Exception, msg:
+        self.send_block(stream.ErrorBlock(0, block, str(msg)))
+        if options.debug: raise
+
+
+
 
 
 if __name__ == '__main__':
