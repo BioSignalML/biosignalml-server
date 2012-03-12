ab3e0d9ecb76138b2646628070784b88006457e6
Dave Brooks <dave@bcs.co.nz>
2012-03-12 17:17:54 +1300

    Generalise code -- output to a generic writer; call a callback function when finished.

--- httpchunked/chunked_handler.py
+++ httpchunked/chunked_handler.py
@@ -4,28 +4,26 @@ Tornado chunked data handler from https://gist.github.com/753992
 
 from tornado import web
 
-from iostream_callback import (
-    Callback,
-    Data,
-    DONE,
-)
 
-from cStringIO import StringIO
+from iostream_callback import Callback, Data, DONE
 
 WAIT_LENGTH = (1, )
 WAIT_CHUNK = (2, )
 
 
 class ChunkedData(Data):
+#=======================
 
-    def __init__(self):
-        self.chunk = StringIO()
+    def __init__(self, writer):
+        self.writer = writer
         self.chunk_length = 0
 
         super(ChunkedData, self).__init__()
 
 
 class LengthCallback(Callback):
+#==============================
+
     start_state = WAIT_LENGTH
 
     def _handle(self, data):
@@ -39,22 +37,26 @@ class LengthCallback(Callback):
 
 
 class DataCallback(Callback):
+#============================
+
     start_state = WAIT_CHUNK
 
     def _handle(self, data):
         assert data[-2:] == '\r\n', "chunk data ends with CRLF"
-        self.data.chunk.write(data[:-2])
+        self.data.writer.write(data[:-2])
 
         self.data.state = WAIT_LENGTH
 
 
 class ChunkReader(object):
-    def __init__(self, handler):
+#=========================
+
+    def __init__(self, handler, writer):
         self.handler = handler
 
         stream = handler.request.connection.stream
 
-        data = ChunkedData()
+        data = ChunkedData(writer)
         func = Callback.make_entry_callback(data, (
                 LengthCallback(data,
                     lambda self: stream.read_until('\r\n', self)),
@@ -66,24 +68,26 @@ class ChunkReader(object):
         func()
 
     def _done_callback(self, data):
-        self.handler._on_chunks(data.chunk)
+        self.handler.got_chunks(data.writer)
 
 
 class ChunkedHandler(web.RequestHandler):
-    def _handle_chunked(self, *args, **kwargs):
+#========================================
+
+    def have_chunked(self, writer, finished):
         # we assume that the wrapping server has not sent/flushed the
         # 100 (Continue) response
         if self.request.headers.get('Expect', None) == '100-continue' and \
             not 'Content-Length' in self.request.headers and \
             self.request.headers.get('Transfer-Encoding', None) == 'chunked':
-
             self._auto_finish = False
-            ChunkReader(self)
-
+            self._finished_callback = finished
+            ChunkReader(self, writer)
             self.request.write("HTTP/1.1 100 (Continue)\r\n\r\n")
-
             return True
         return False
 
-    def _on_chunks(self, all_chunks):
+    def got_chunks(self, writer):
+        if self._finished_callback:
+          self._finished_callback(writer)
         self.finish()
