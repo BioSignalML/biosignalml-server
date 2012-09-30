601005ef4d6380b82ad64872c5b9c3bb1c941d9b
Dave Brooks <dave@bcs.co.nz>
2012-09-30 22:38:10 +1300

    WIP -- specify class of graph we are keeping provenance of instead of assuming it's a 'rdfg:Graph'.

--- triplestore/provenance.py
+++ triplestore/provenance.py
@@ -10,7 +10,7 @@
 
 
 from biosignalml.rdf import Uri, Graph, Statement
-from biosignalml.rdf import RDF, RDFG, PRV, DCTERMS
+from biosignalml.rdf import RDF, PRV, DCTERMS
 from biosignalml.utils import utctime_as_string
 
 
@@ -22,11 +22,11 @@ class Provenance(Graph):
     Graph.__init__(self, uri)
 
 
-  def add_graph(self, subject, agent, ancestor=None):
-  #--------------------------------------------------
+  def add_graph(self, type, subject, agent, ancestor=None):
+  #--------------------------------------------------------
     graph = self.uri.make_uri()
     self.append(Statement(graph, RDF.type, PRV.DataItem))
-    self.append(Statement(graph, RDF.type, RDFG.Graph))
+    self.append(Statement(graph, RDF.type, type))
     self.append(Statement(graph, DCTERMS.subject, subject))
     if ancestor: self.append(Statement(graph, PRV.precededBy, ancestor))
     createdby = self.uri.make_uri()
