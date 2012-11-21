f3f6a63203831e66ca846327d613acb20fc88e57
Dave Brooks <dave@bcs.co.nz>
2012-11-22 09:39:52 +1300

    Add repository URL as a required parameter to 'delete_recording' and 'reset' utilities.

--- delete_recording.py
+++ delete_recording.py
@@ -11,29 +11,32 @@ if __name__ == '__main__':
 
   store = Virtuoso('http://localhost:8890')
 
-  if len(sys.argv) < 2:
-    print "Usage: %s recording_uri" % sys.argv[0]
+  if len(sys.argv) < 3:
+    print "Usage: %s repository recording_uri" % sys.argv[0]
     exit(1)
+
+  (repo, recording) = tuple(sys.argv[1:3])
 
   for g in [ r['g']['value'] for r in json.loads(
                store.query("""select distinct ?g where {
-                               graph <http://devel.biosignalml.org/provenance> {
+                               graph <%s/provenance> {
                                 ?g a bsml:RecordingGraph ;
                                    dct:subject <%s>
                                 }
-                              } order by ?g""" % sys.argv[1], format=Format.JSON)
+                              } order by ?g""" % (repo, recording), format=Format.JSON)
                ).get('results', {}).get('bindings', [])
             ]:  store.delete_graph(g)
-  store.query("""WITH <http://devel.biosignalml.org/provenance>
+
+  store.query("""WITH <%s/provenance>
                  DELETE { ?x ?p ?v } WHERE {
                    ?g a bsml:RecordingGraph ;
                       dct:subject <%s> ;
                       prv:createdBy ?x .
                    ?x ?p ?v .
-                   }""" % sys.argv[1])
-  store.query("""WITH <http://devel.biosignalml.org/provenance>
+                   }""" % (repo, recording))
+  store.query("""WITH <%s/provenance>
                  DELETE { ?g ?p ?v } WHERE {
                    ?g a bsml:RecordingGraph ;
                       dct:subject <%s> ;
                       ?p ?v .
-                   }""" % sys.argv[1])
+                   }""" % (repo, recording))
