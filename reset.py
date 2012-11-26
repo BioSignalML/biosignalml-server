aae0cd93eceddc0f2e47ee4ff58e99b590da6840
Dave Brooks <dave@bcs.co.nz>
2012-11-27 12:28:33 +1300

    Specify the repository as a parameter to the 'reset.py' utility.

--- reset.py
+++ reset.py
@@ -9,8 +9,14 @@ from biosignalml.repository import BSMLStore
 if __name__ == '__main__':
 #=========================
 
+  if len(sys.argv) < 2:
+    print "Usage: %s repository" % sys.argv[0]
+    exit(1)
+
+  repo = sys.argv[1]
+
   print "***"
-  print "*** This utility will remove all BioSignalML content from the repository ***"
+  print "*** This utility will remove all BioSignalML content from the repository at %s ***" % repo
   print "***"
   print "*** Are your sure? [NO/yes]"
   if sys.stdin.readline() != "yes\n": sys.exit(1)
@@ -22,18 +28,19 @@ if __name__ == '__main__':
 
   for g in [ r['g']['value'] for r in json.loads(
                store.query("""select distinct ?g where {
-                               graph <http://devel.biosignalml.org/provenance> {
+                               graph <%s/provenance> {
                                 ?g a bsml:RecordingGraph
                                 }
-                              } order by ?g""", format=Format.JSON)
+                              } order by ?g""" % repo, format=Format.JSON)
                ).get('results', {}).get('bindings', [])
             ]:  store.delete_graph(g)
-  store.query("""WITH <http://devel.biosignalml.org/provenance>
+
+  store.query("""WITH <%s/provenance>
                  DELETE { ?x ?p ?v } WHERE {
                    ?g a bsml:RecordingGraph ; prv:createdBy ?x .
                    ?x ?p ?v .
-                   }""")
-  store.query("""WITH <http://devel.biosignalml.org/provenance>
+                   }""" % repo)
+  store.query("""WITH <%s/provenance>
                  DELETE { ?g ?p ?v } WHERE {
                    ?g a bsml:RecordingGraph ; ?p ?v .
-                   }""")
+                   }""" % repo)
