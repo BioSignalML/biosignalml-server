3b005719ac5f05e5a0adc252189dd9284e1c7b52
Dave Brooks <dave@bcs.co.nz>
2013-01-11 15:29:19 +1300

    Improve code readability.

--- endpoints/sparql.py
+++ endpoints/sparql.py
@@ -12,18 +12,22 @@ class sparql(tornado.web.RequestHandler):
 #==========================================
 
   def check_xsrf_cookie(self):
+  #---------------------------
     pass
   
   def header_handler(self, header):
+  #--------------------------------
     hdr = header.split(':', 1)
     self.set_header(hdr[0], hdr[1].strip())
     
   def stream_handler(self, response):
+  #----------------------------------
     if len(response):
       self.write(response)
       self.flush()
     
   def request_handler(self, response):
+  #-----------------------------------
     if response.code != 599: self.set_status(response.code)
     else:                    self.set_status(504)  # gateway timeout
     self.finish()
@@ -31,6 +35,7 @@ class sparql(tornado.web.RequestHandler):
     self.http_client.close()
 
   def do_query(self, body=None):
+  #-----------------------------
     self.ioloop = ioloop.IOLoop()
     self.http_client = httpclient.AsyncHTTPClient(self.ioloop)
     self.http_client.fetch(
@@ -44,7 +49,9 @@ class sparql(tornado.web.RequestHandler):
     self.ioloop.start()
 
   def get(self):
+  #-------------
     self.do_query()
   
   def post(self):
+  #--------------
     self.do_query(self.request.body)
