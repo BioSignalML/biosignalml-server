c5f586c34675aee61e11dd32426e0211931d007d
Dave Brooks <dave@bcs.co.nz>
2013-03-06 11:55:05 +1300

    Modify 'load_units.sh' to reload all unit ontologies; add 'load_tags.sh' to (re)load semantic tags.

---
+++ tools/load_tags.sh
@@ -0,0 +1,3 @@
+python ./delete_graph.py http://localhost:8890 http://ontologies.biosignalml.org/semantic-tags
+python ./load_graph.py   http://localhost:8890 http://ontologies.biosignalml.org/semantic-tags  \
+  http://www.biosignalml.org/ontologies/examples/sleep
--- tools/load_units.sh
+++ tools/load_units.sh
@@ -1,1 +1,5 @@
-python ~/biosignalml/python/utils/repository/load_graph.py http://localhost:8890 http://ontologies.biosignalml.org/units $1
+python ./delete_graph.py http://localhost:8890 http://ontologies.biosignalml.org/units
+python ./load_graph.py   http://localhost:8890 http://ontologies.biosignalml.org/units  \
+  http://www.sbpax.org/uome/list.owl
+python ./load_graph.py   http://localhost:8890 http://ontologies.biosignalml.org/units  \
+  http://www.biosignalml.org/ontologies/examples/unit
