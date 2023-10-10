0aa9ebbff57c4588691f0f22ffd8029a39af572d
David Brooks <d.brooks@auckland.ac.nz>
2023-10-11 11:54:26 +1300

    Update Virtuoso setup scripts -- we now have UOME ontology on biosignalml.org.

--- tools/load_tags.sh
+++ tools/load_tags.sh
@@ -1,3 +1,3 @@
 python ./delete_graph.py http://virtuoso:8890 http://ontologies.biosignalml.org/semantic-tags
 python ./load_graph.py   http://virtuoso:8890 http://ontologies.biosignalml.org/semantic-tags  \
-  http://www.biosignalml.org/ontologies/examples/sleep
+  https://www.biosignalml.org/ontologies/examples/sleep
--- tools/load_units.sh
+++ tools/load_units.sh
@@ -1,5 +1,5 @@
 python ./delete_graph.py http://virtuoso:8890 http://ontologies.biosignalml.org/units
 python ./load_graph.py   http://virtuoso:8890 http://ontologies.biosignalml.org/units  \
-  http://www.sbpax.org/uome/list.owl
+  https://www.biosignalml.org/ontologies/uome/list.n3 turtle
 python ./load_graph.py   http://virtuoso:8890 http://ontologies.biosignalml.org/units  \
-  http://www.biosignalml.org/ontologies/examples/unit
+  https://www.biosignalml.org/ontologies/examples/unit
